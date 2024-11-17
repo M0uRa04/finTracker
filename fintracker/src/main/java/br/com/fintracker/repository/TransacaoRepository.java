32f8525a93b23bc0b64a593b2ed470d72e9d0d54
Robson <robsonmoura970@hotmail.com>
2024-11-17 06:33:58 -0300

    Feat: Implementando a interfacee de persistência de dados da class transacao

---
+++ fintracker/src/main/java/br/com/fintracker/repository/TransacaoRepository.java
@@ -0,0 +1,7 @@
+package br.com.fintracker.repository;
+
+import br.com.fintracker.model.Transacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransacaoRepository extends JpaRepository <Transacao, Long> {
+}
