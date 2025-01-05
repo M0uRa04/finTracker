66709c7444c6a0341e9157aa209d52fc8f7ab373
Robson <robsonmoura970@hotmail.com>
2025-01-05 07:18:54 -0300

    Feature: Implementando a interface CrudService em TransacaoService

--- fintracker/src/main/java/br/com/fintracker/service/TransacaoService.java
+++ fintracker/src/main/java/br/com/fintracker/service/TransacaoService.java
@@ -1,8 +1,43 @@
 package br.com.fintracker.service;
 
+import br.com.fintracker.dto.transacao.DadosAtualizacaoTransacao;
+import br.com.fintracker.dto.transacao.DadosCadastroTransacao;
+import br.com.fintracker.dto.transacao.DadosRespostaTransacao;
 import org.springframework.stereotype.Service;
 
-@Service
-public class TransacaoService  {
+import java.util.List;
+import java.util.Optional;
 
+@Service
+public class TransacaoService implements CrudService <DadosRespostaTransacao, DadosCadastroTransacao, DadosAtualizacaoTransacao> {
+
+    @Override
+    public DadosRespostaTransacao inserirNoBancoDeDados(DadosCadastroTransacao dadosCadastro) {
+        return null;
+    }
+
+    @Override
+    public Optional<DadosRespostaTransacao> buscarPorId(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<DadosRespostaTransacao> listarTodos() {
+        return null;
+    }
+
+    @Override
+    public Optional<DadosRespostaTransacao> atualizar(Long id, DadosAtualizacaoTransacao dadosAtualizacao) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void inativar(Long id) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
 }
