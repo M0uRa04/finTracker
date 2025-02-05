4e261b87a9407d327392e4605e7792d792c0a17d
Robson <robsonmoura970@hotmail.com>
2025-02-05 19:23:57 -0300

    Feature: Criando a classe filha RelatórioResumoCotas

--- fintracker/src/main/java/br/com/fintracker/model/relatorio/RelatorioResumoCotas.java
+++ fintracker/src/main/java/br/com/fintracker/model/relatorio/RelatorioResumoCotas.java
@@ -1,2 +1,20 @@
-package br.com.fintracker.model.relatorio;public class RelatorioResumoCotas {
+package br.com.fintracker.model.relatorio;
+
+import br.com.fintracker.model.categoria.Categoria;
+import br.com.fintracker.model.categoria.StatusAtingimentoCota;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "relatorio_resumo_cotas")
+public class RelatorioResumoCotas extends Relatorio {
+
+
+    private Categoria categoria;
+    private BigDecimal totalGasto;
+    private Float porcentagemAtingimento;
+    private StatusAtingimentoCota statusAtingimentoCota;
+
 }
