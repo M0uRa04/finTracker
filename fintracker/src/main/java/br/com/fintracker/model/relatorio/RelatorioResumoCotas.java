c01219a82b3904259a489a6a0fb41331abbcc18d
Robson <robsonmoura970@hotmail.com>
2025-02-05 19:35:50 -0300

    Feature: Incluindo os construtores, getters e setters e Equals And HashCode na classe RelatorioResumoCotas

--- fintracker/src/main/java/br/com/fintracker/model/relatorio/RelatorioResumoCotas.java
+++ fintracker/src/main/java/br/com/fintracker/model/relatorio/RelatorioResumoCotas.java
@@ -2,10 +2,13 @@ package br.com.fintracker.model.relatorio;
 
 import br.com.fintracker.model.categoria.Categoria;
 import br.com.fintracker.model.categoria.StatusAtingimentoCota;
+import br.com.fintracker.model.usuario.Usuario;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "relatorio_resumo_cotas")
@@ -21,4 +24,66 @@ public class RelatorioResumoCotas extends Relatorio {
     @Enumerated(value = EnumType.STRING)
     private StatusAtingimentoCota statusAtingimentoCota;
 
+    public RelatorioResumoCotas () {}
+
+    public RelatorioResumoCotas(List<Categoria> categorias, BigDecimal totalGasto, Float porcentagemAtingimento, StatusAtingimentoCota statusAtingimentoCota) {
+        this.categorias = categorias;
+        this.totalGasto = totalGasto;
+        this.porcentagemAtingimento = porcentagemAtingimento;
+        this.statusAtingimentoCota = statusAtingimentoCota;
+    }
+
+    public RelatorioResumoCotas(Long id, TipoRelatorio tipoRelatorio, LocalDate dataInicio, LocalDate dataFim, Usuario usuario, List<Categoria> categorias, BigDecimal totalGasto, Float porcentagemAtingimento, StatusAtingimentoCota statusAtingimentoCota) {
+        super(id, tipoRelatorio, dataInicio, dataFim, usuario);
+        this.categorias = categorias;
+        this.totalGasto = totalGasto;
+        this.porcentagemAtingimento = porcentagemAtingimento;
+        this.statusAtingimentoCota = statusAtingimentoCota;
+    }
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
+    }
+
+    public BigDecimal getTotalGasto() {
+        return totalGasto;
+    }
+
+    public void setTotalGasto(BigDecimal totalGasto) {
+        this.totalGasto = totalGasto;
+    }
+
+    public Float getPorcentagemAtingimento() {
+        return porcentagemAtingimento;
+    }
+
+    public void setPorcentagemAtingimento(Float porcentagemAtingimento) {
+        this.porcentagemAtingimento = porcentagemAtingimento;
+    }
+
+    public StatusAtingimentoCota getStatusAtingimentoCota() {
+        return statusAtingimentoCota;
+    }
+
+    public void setStatusAtingimentoCota(StatusAtingimentoCota statusAtingimentoCota) {
+        this.statusAtingimentoCota = statusAtingimentoCota;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        RelatorioResumoCotas that = (RelatorioResumoCotas) o;
+        return Objects.equals(getCategorias(), that.getCategorias()) && Objects.equals(getTotalGasto(), that.getTotalGasto()) && Objects.equals(getPorcentagemAtingimento(), that.getPorcentagemAtingimento()) && getStatusAtingimentoCota() == that.getStatusAtingimentoCota();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getCategorias(), getTotalGasto(), getPorcentagemAtingimento(), getStatusAtingimentoCota());
+    }
 }
