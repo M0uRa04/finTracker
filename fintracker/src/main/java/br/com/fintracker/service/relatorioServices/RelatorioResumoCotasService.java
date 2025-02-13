599602ea92afa60ba64c94b7b8d695eb93a7c823
Robson <robsonmoura970@hotmail.com>
2025-02-13 19:32:22 -0300

    Feature: Criando a estrutura de pacotes dos serviços e dos repositorios dos relatórios

---
+++ fintracker/src/main/java/br/com/fintracker/service/relatorioServices/RelatorioResumoCotasService.java
@@ -0,0 +1,11 @@
+package br.com.fintracker.service.relatorioServices;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RelatorioResumoCotasService {
+
+    public void atualizarRelatorio () {
+    }
+
+}
