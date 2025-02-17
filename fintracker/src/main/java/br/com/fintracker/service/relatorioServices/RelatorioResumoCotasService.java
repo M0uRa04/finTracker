d72e9a1cba96786ea75f3703052878fae7c61794
Robson Moura <robsonmouradev@970@gmail.com>
2025-02-17 14:22:14 -0300

    Emergency commit

--- fintracker/src/main/java/br/com/fintracker/service/relatorioServices/RelatorioResumoCotasService.java
+++ fintracker/src/main/java/br/com/fintracker/service/relatorioServices/RelatorioResumoCotasService.java
@@ -1,11 +1,14 @@
 package br.com.fintracker.service.relatorioServices;
 
+import java.math.BigDecimal;
+
 import br.com.fintracker.dto.relatorio.relatorioatingimentocotas.RangeDatasRelatorioDTO;
 import br.com.fintracker.dto.relatorio.relatorioatingimentocotas.TotalGastoPorCategoriaDTO;
 import br.com.fintracker.infra.security.UserContext;
 import br.com.fintracker.model.categoria.StatusAtingimentoCota;
 import br.com.fintracker.model.relatorio.RelatorioResumoCotas;
 import br.com.fintracker.repository.relatorioRepositories.RelatorioResumoCotasRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +20,6 @@ public class RelatorioResumoCotasService {
 
     @Autowired
     private RelatorioResumoCotasRepository relatorioResumoCotasRepository;
-
-    private List<TotalGastoPorCategoriaDTO> obterTotalGastoPorCategoria (RangeDatasRelatorioDTO dto) {
-        return  relatorioResumoCotasRepository.calculaTotalGastoPorCategoria(UserContext.getUserId(), dto.dataInicio(), dto.dataFim());
-    }
 
     public List <RelatorioResumoCotas> criarRelatorioResumoCotas (RangeDatasRelatorioDTO rangeDatasRelatorioDTO) {
         var totaisPorCategoria = obterTotalGastoPorCategoria(rangeDatasRelatorioDTO);
@@ -35,4 +34,13 @@ public class RelatorioResumoCotasService {
                         StatusAtingimentoCota.DEFAULT))
                 .collect(Collectors.toList());
     }
-}
+
+    
+    private List<TotalGastoPorCategoriaDTO> obterTotalGastoPorCategoria (RangeDatasRelatorioDTO dto) {
+        return  relatorioResumoCotasRepository.calculaTotalGastoPorCategoria(UserContext.getUserId(), dto.dataInicio(), dto.dataFim());
+    }
+
+    private Float calculaPorcentagemAtingimento (BigDecimal cota, BigDecimal totalGasto) {
+
+    }
+ }
